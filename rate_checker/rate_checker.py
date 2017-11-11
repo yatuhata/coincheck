134fefcbde6c5d1ab788ac09fab375f1d9d3d189
yatuhata <katsura394@gmail.com>
2017-11-11 22:44:39 +0900

    実行されたときにローカルのinfluxDBに現在のrateを書き込む

--- rate_checker/rate_checker.py
+++ rate_checker/rate_checker.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 import time
+import datetime
 import requests
+from influxdb import InfluxDBClient
 
 
 class CoincheckClient:
@@ -10,12 +12,30 @@ class CoincheckClient:
     URL = 'https://coincheck.com/api'
 
     def get_rate(self):
-        rate = requests.get(self.URL + '/rate/btc_jpy').json()
-        print('%s' % rate)
+        return requests.get(self.URL + '/rate/btc_jpy').json()
 
 
-# Check rate with Coincheck API per 1 seconds
-client = CoincheckClient()
-while True:
-    client.get_rate()
-    time.sleep(1)
+if __name__ == '__main__':
+    # Setting
+    coincheck_client = CoincheckClient()
+    influxdb_client = InfluxDBClient(
+        host='localhost',
+        port=8086,
+        username='root',
+        password='root',
+        database='coincheck'
+    )
+    influxdb_client.create_database('coincheck')
+
+    # Check rate with Coincheck API
+    data = coincheck_client.get_rate()
+    json_body = [
+        {
+            "measurement": "rate",
+            "time": datetime.datetime.now().isoformat('T'),
+            "fields": {
+                "btc_jpy": float(data['rate'])
+            }
+        }
+    ]
+    influxdb_client.write_points(json_body)
